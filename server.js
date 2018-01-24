dff662bd5d60b62504d463f6a91fac13cb006f7a
Ankit Sharma <ankitkumarsharma939@gmail.com>
2018-01-24 13:08:35 +0530

    asdf

--- server.js
+++ server.js
@@ -13,11 +13,13 @@
 	app.use(config);
 	app.use(express.static(__dirname + '/app'));
   	app.use(compression());
+  	app.set('views', __dirname + '/app');
+  	app.engine('html', require('ejs').renderFile);
 
 	app.post('/api/reservation', reservationMiddleware);
 	app.post('/api/order', orderMiddleware);
 
-	app.get('/:slag(^api)', function(req, res, next) {
+	app.all('/*', function(req, res, next) {
     	res.render('index.html', { root: __dirname });
 	});
 
