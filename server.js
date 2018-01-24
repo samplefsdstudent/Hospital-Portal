ea19fbd42772e0645151a6696d589b952c3173be
Ankit Sharma <ankitkumarsharma939@gmail.com>
2018-01-24 15:07:26 +0530

    sdfsaf

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 	var reservationMiddleware = require('./middlewares/reservation'),
+		nodemailer = require('nodemailer'),
     	orderMiddleware = require('./middlewares/order'),
     	compression = require('compression'),
     	config = require('./config/config'),
@@ -9,10 +10,39 @@
     const port = process.env.PORT || 5000;
 
 	mongoose.connect('mongodb://samplefsd:ankit939@ds213118.mlab.com:13118/restaurant')
+	
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({extended : false}));
 	app.use(config);
   	app.use(compression());
+
+  	let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+	    secure : false,
+	    port : 587,
+      auth:{
+        user: 'samplefsdstudent@gmail.com',
+        pass:'ankit939'
+      },
+	    tls:{
+		    rejectUnauthorized : false
+	     }
+    });
+
+    var mailOptions = {
+        from: '"Sample FSD Project" <samplefsdstudent@gmail.com>',
+        to: 'ankitkumarsharma939@gmail.com',
+        subject: 'FSD Server is Up',
+        text: 'FSD Server is Up'
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }else{
+        	console.log(info);
+        }
+    });
 
 	app.post('/api/reservation', reservationMiddleware);
 	app.post('/api/order', orderMiddleware);
