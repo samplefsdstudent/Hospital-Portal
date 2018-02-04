e3806e638d3ac9695f68c97c04f3ed090aa4360f
Ankit Sharma <ankitkumarsharma939@gmail.com>
2018-02-04 12:09:31 +0530

    Final

--- app/assets/controllers/contact_usCtrl.js
+++ app/assets/controllers/contact_usCtrl.js
@@ -1,5 +1,5 @@
-angular.module('myApp').controller('contact_usCtrl', ['anchorScroll','$scope','prefix_url','$http', function($routeParams,$scope,prefix_url,$http){
-	$scope.sendFeeback = function(data){
+angular.module('myApp').controller('contact_usCtrl', ['$anchorScroll','$scope','prefix_url','$http', function($anchorScroll,$scope,prefix_url,$http){
+	$scope.sendFeedback = function(data){
 		data.date = new Date();
         $http.post(prefix_url + 'feedback', data).then(function(data){
             alert('Received Feedback! Thank you for precious time to send us feedback. ')
